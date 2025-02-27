1f8a6ba06e16695ce4308135738a7dd846b86bdf
Jaid-Khan <jaidkhan04072001@gmail.com>
2025-02-27 15:06:37 +0530

    created nav bar

---
+++ src/script.js
@@ -0,0 +1,11 @@
+document.getElementById("hamburger").addEventListener("click", function () {
+    const mobileMenu = document.getElementById("mobile-menu");
+    if (mobileMenu.classList.contains("hidden")) {
+        mobileMenu.classList.remove("hidden");
+        mobileMenu.classList.add("flex");
+    } else {
+        mobileMenu.classList.add("hidden");
+        mobileMenu.classList.remove("flex");
+    }
+});
+
