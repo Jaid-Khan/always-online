5fb0508ba0dd5ecb0956997b017fa342b0e0e10b
JAID-KHAN <jaidkhan04072001@gmail.com>
2025-07-20 00:14:02 +0530

    update nav, hero button, nave color hover effect and added and updated some more services

--- src/script.js
+++ src/script.js
@@ -1,11 +1,6 @@
-// HAMBURGER MENU SHO AND HIDE FUNCTION 
-document.getElementById("hamburger").addEventListener("click", function () {
-  const mobileMenu = document.getElementById("mobile-menu");
-  if (mobileMenu.classList.contains("hidden")) {
-      mobileMenu.classList.remove("hidden");
-      mobileMenu.classList.add("flex");
-  } else {
-      mobileMenu.classList.add("hidden");
-      mobileMenu.classList.remove("flex");
-  }
-});
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
